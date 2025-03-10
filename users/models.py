bf8abca2528ab93ef0206f66cd658c17f4e15302
Daria <spaceandworld3421@mail.ru>
2025-03-10 14:44:30 +0300

    Добавление моделей Company и Department.
    - Добавлен класс Company для управления компаниями.
    - Добавлен класс Department для управления отделами.
    - Добавлены комментарии к моделям для лучшего понимания их назначения.
    - Обновлен CustomUser: заменено поле department с CharField на ForeignKey(Department).

--- users/models.py
+++ users/models.py
@@ -3,11 +3,18 @@ from django.contrib.auth.models import AbstractUser
 
 
 class CustomUser(AbstractUser):
+    """Модель пользователя с расширенными полями"""
     ROLE_CHOICES = [
         ('manager', 'Руководитель'),
         ('employee', 'Сотрудник')
     ]
-    department = models.CharField(max_length=255, null=True, blank=True, verbose_name="Отдел")
+    department = models.ForeignKey(
+        'Department', 
+        on_delete=models.SET_NULL, 
+        null=True, 
+        blank=True, 
+        verbose_name="Отдел"
+    )
     manager = models.ForeignKey(
         'self', 
         on_delete=models.SET_NULL, 
@@ -25,3 +32,31 @@ class CustomUser(AbstractUser):
     class Meta:
         verbose_name = "Пользователь"
         verbose_name_plural = "Пользователи"
+
+
+class Company(models.Model):
+    """Модель компании"""
+    name = models.CharField(verbose_name='Название компании', max_length=255, unique=True)
+    description = models.TextField(verbose_name='Описание', blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Department(models.Model):
+    """Модель отделов"""
+    title = models.CharField(verbose_name='Название отдела', max_length=100, unique=True)
+    company = models.ForeignKey(
+        Company, 
+        on_delete=models.CASCADE, 
+        verbose_name='Компания', 
+        blank=True, 
+        null=True
+    )
+
+    def __str__(self):
+        return f"{self.title} ({self.company.name if self.company else 'Нет компании'})"
+
+    class Meta:
+        verbose_name = "Отдел"
+        verbose_name_plural = "Отделы"
