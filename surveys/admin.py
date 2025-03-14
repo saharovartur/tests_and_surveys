f3178112dfc5b7a7fa153409cd7fe3b1702629eb
Daria <spaceandworld3421@mail.ru>
2025-03-14 23:43:33 +0300

    Настройка админки

--- surveys/admin.py
+++ surveys/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from .models import Survey, SurveyQuestion, SurveyAnswerOption, SurveyResponse, SurveyAnswer
 
-# Register your models here.
+
+@admin.register(Survey)
+class SurveyAdmin(admin.ModelAdmin):
+    list_display = ('title', 'description', 'created_by', 'company', 'department', 'created_at', 'recipient_email')
+
+
+@admin.register(SurveyQuestion)
+class SurveyQuestionAdmin(admin.ModelAdmin):
+    list_display = ('survey', 'text')
+
+
+@admin.register(SurveyAnswerOption)
+class SurveyAnswerOptionAdmin(admin.ModelAdmin):
+    list_display = ('question', 'text')
+
+
+@admin.register(SurveyResponse)
+class SurveyResponseAdmin(admin.ModelAdmin):
+    list_display = ('survey', 'user', 'submitted_at')
+
+
+@admin.register(SurveyAnswer)
+class SurveyAnswerAdmin(admin.ModelAdmin):
+    list_display = ('response', 'question', 'selected_option')
--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Company, Department, CustomUser
 
-# Register your models here.
+@admin.register(CustomUser)
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'email', 'role', 'department', 'manager')
+
+
+@admin.register(Company)
+class CompanyAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description', 'founded_year', 'leader')
+
+
+@admin.register(Department)
+class DepartmentAdmin(admin.ModelAdmin):
+    list_display = ('title', 'company', 'leader')
